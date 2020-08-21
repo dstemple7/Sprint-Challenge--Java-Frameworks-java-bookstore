ade6c09e4e42166eeca85e4cfeea47af92208d3a
David Temple <dstemple7@gmail.com>
2020-08-21 12:28:17 -0500

    mvp

--- bookstore/src/main/java/com/lambdaschool/bookstore/controllers/OpenController.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/controllers/OpenController.java
@@ -6,18 +6,14 @@ import com.lambdaschool.bookstore.models.UserRoles;
 import com.lambdaschool.bookstore.services.RoleService;
 import com.lambdaschool.bookstore.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -90,6 +86,9 @@ public class OpenController
                 .buildAndExpand(newuser.getUserid())
                 .toUri();
         responseHeaders.setLocation(newUserURI);
+
+
+        RestTemplate restTemplate = new RestTemplate();
 
         // return the access token
         // To get the access token, surf to the endpoint /login just as if a client had done this.
--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,6 +2,8 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Section;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
@@ -22,11 +28,20 @@ public class BookServiceImplTest
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private SectionService sectionService;
+
     @Before
     public void setUp() throws
             Exception
     {
         MockitoAnnotations.initMocks(this);
+
+        List<Book> myList = bookService.findAll();
+        for (Book b : myList)
+        {
+            System.out.println(b.getBookid() + " " + b.getTitle());
+        }
     }
 
     @After
@@ -38,26 +53,38 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        assertEquals("test Flatterland", bookService.findBookById(26).getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
+        bookService.findBookById(666);
+
     }
 
     @Test
     public void delete()
     {
+        bookService.delete(28);
+        assertEquals(4, bookService.findAll().size());
     }
 
     @Test
     public void save()
     {
+        Section sTest = new Section("Save Test");
+        sTest = sectionService.save(sTest);
+        Book b = new Book("David's First Book", "666616666", 2084,sTest);
+        bookService.save(b);
+
+        assertEquals(6, bookService.findAll().size());
     }
 
     @Test
